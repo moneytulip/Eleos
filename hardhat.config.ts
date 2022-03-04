fef53a313bd6f402da0b53a8186e126e6889d495
oregano <flimflam@gmail.com>
2022-03-04 18:17:10 +0000

    Cleanup deployment, add oasis rpcs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,19 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
-import {HardhatUserConfig, HttpNetworkConfig, HttpNetworkHDAccountsConfig} from 'hardhat/types';
-import '@typechain/hardhat'
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-waffle'
-import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-truffle5';
+import {
+  HardhatUserConfig,
+  HttpNetworkConfig,
+  HttpNetworkHDAccountsConfig,
+} from "hardhat/types";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-truffle5";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
@@ -17,27 +21,20 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
-    // fantom: {
-    //   chainId: 250,
-    //   url: 'https://rpcapi.fantom.network',
-    //   accounts: {
-    //     mnemonic: process.env.MNEMONIC
-    //   }
-    // },
-    // fantomtestnet: {
-    //   chainId: 4002,
-    //   url: 'https://rpc.testnet.fantom.network',
-    //   accounts: {
-    //     mnemonic: process.env.MNEMONIC
-    //   }
-    // }
+    oasistest: {
+      chainId: 42261,
+      url: "https://testnet.emerald.oasis.dev",
+      accounts: [process.env.OASIS_PRIV_KEY],
+    },
+    oasis: {
+      chainId: 42262,
+      url: "https://emerald.oasis.dev",
+      accounts: [process.env.OASIS_PRIV_KEY],
+    },
   },
-  // etherscan: {
-  //   apiKey: process.env.FTMSCAN_API_KEY
-  // }
 };
 
 export default config;
